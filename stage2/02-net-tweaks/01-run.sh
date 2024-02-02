c66cd85ea428cd981a434e1404cda30daa91f392
Nacho Carballeda <icarballeda@sentrisense.com>
2024-02-02 17:59:33 -0300

    host no se conecta a wifi usando credenciales..
    tambien probe instalando dhcp y no funciono

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -3,20 +3,12 @@
 install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
 
-cat << EOF > "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf"
-country=${WPA_COUNTRY}
-
-network={
-    ssid="${WIFI_SSID}"
-    psk="${WIFI_PSWD}"
-}
-EOF
-
 if [ -v WPA_COUNTRY ]; then
 	on_chroot <<- EOF
 		SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_wifi_country "${WPA_COUNTRY}"
 	EOF
 fi
+
 
 # Disable wifi on 5GHz models if WPA_COUNTRY is not set
 mkdir -p "${ROOTFS_DIR}/var/lib/systemd/rfkill/"
--- stage3/00-netbird/01.run-chroot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-RUNLEVEL=1 apt-get install netbird
