8a7906f3b9e518295436e26dccb1fde7733a27e1
Nacho Carballeda <icarballeda@sentrisense.com>
2024-03-01 17:30:27 -0300

    feat(stage3): install labgrid coordinator

---
+++ stage3/01-labgrid/00-run-chroot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+cd /home/$FIRST_USER_NAME
+
+git clone https://github.com/labgrid-project/labgrid
+
+cd labgrid
+
+virtualenv -p python3 crossbar-venv
+crossbar-venv/bin/pip install --upgrade pip
+crossbar-venv/bin/pip install -r crossbar-requirements.txt
+
+virtualenv -p python3 labgrid-venv
+source labgrid-venv/bin/activate
+
+pip install --upgrade pip
+pip install .
+
+
--- stage3/03-install-services/00-run.sh
+++ stage3/03-install-services/00-run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+for file in files/*; do
+    if [ -f "$file" ]; then
+        sed -i "s/example_user/$FIRST_USER_NAME/g" "$file"
+    fi
+done
+
 install -v -d					"${ROOTFS_DIR}/etc/labgrid"
 install -v -m 600 files/exporter.yaml	"${ROOTFS_DIR}/etc/labgrid/"
 install -v -m 600 files/coordinator.yaml	"${ROOTFS_DIR}/etc/labgrid/"
@@ -7,6 +13,10 @@ install -v -m 600 files/coordinator.yaml	"${ROOTFS_DIR}/etc/labgrid/"
 install -v -m 600 files/labgrid-coordinator.service	"${ROOTFS_DIR}/etc/systemd/system/"
 install -v -m 600 files/labgrid-exporter.service	"${ROOTFS_DIR}/etc/systemd/system/"
 
-if [ -f "${ROOTFS_DIR}/etc/systemd/system/labgrid-exporter.service" ]; then
-    sed -i "s/User=.*/User=$FIRST_USER_NAME/" 
-fi
+on_chroot << EOF
+    systemctl enable labgrid-coordinator.service
+EOF
+
+on_chroot << EOF
+    systemctl enable labgrid-exporter.service
+EOF
