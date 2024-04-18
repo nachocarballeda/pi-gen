4aa622e35bbc4386c64dc56a5c4cb5e134e78e7a
Nacho Carballeda <icarballeda@sentrisense.com>
2024-04-18 18:15:03 -0300

    generate ssh pair & copy to known hosts

--- stage3/00-netbird/00-run-chroot.sh
+++ stage3/00-netbird/00-run-chroot.sh
@@ -6,3 +6,13 @@ rm -f /usr/share/keyrings/netbird-archive-keyring.gpg
 curl -sSL https://pkgs.netbird.io/debian/public.key | gpg --batch --dearmor --output /usr/share/keyrings/netbird-archive-keyring.gpg
 echo 'deb [signed-by=/usr/share/keyrings/netbird-archive-keyring.gpg] https://pkgs.netbird.io/debian stable main' | tee /etc/apt/sources.list.d/netbird.list
 apt-get update
+
+# Generate SSH key pair
+ssh-keygen -t rsa -b 4096 -C "$FIRST_USER_NAME@sentrisense.com" <<< y
+
+# Get the public key
+public_key=$(cat ~/.ssh/id_rsa.pub)
+
+# Copy the public key to remote host's known_hosts file
+mkdir -p ~/.ssh && echo \"$public_key\" >> ~/.ssh/authorized_keys
+
