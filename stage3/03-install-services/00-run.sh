c1770bf887582fdf7ed143fb4dfdb94fb7e81a10
Nacho Carballeda <icarballeda@sentrisense.com>
2024-07-03 17:35:52 -0300

    add tmux layout for labgrid & service

--- stage3/03-install-services/00-run.sh
+++ stage3/03-install-services/00-run.sh
@@ -6,15 +6,17 @@ for file in files/*; do
     fi
 done
 
-install -v -d					"${ROOTFS_DIR}/etc/labgrid"
-install -v -m 600 files/exporter.yaml	"${ROOTFS_DIR}/etc/labgrid/"
-install -v -m 600 files/coordinator.yaml	"${ROOTFS_DIR}/etc/labgrid/"
+install -v -d                               "${ROOTFS_DIR}/etc/labgrid"
+install -v -m 600 files/exporter.yaml       "${ROOTFS_DIR}/etc/labgrid/"
+install -v -m 600 files/coordinator.yaml    "${ROOTFS_DIR}/etc/labgrid/"
+install -v -m 600 files/tmux-labgrid.sh     "${ROOTFS_DIR}/home/$FIRST_USER_NAME/"
 
 install -v -m 600 -D files/sysusers.d/*	"${ROOTFS_DIR}/etc/"
 install -v -m 600 -D files/tmpfiles.d/*	"${ROOTFS_DIR}/etc/"
 
 install -v -m 600 files/labgrid-coordinator.service	"${ROOTFS_DIR}/etc/systemd/system/"
 install -v -m 600 files/labgrid-exporter.service	"${ROOTFS_DIR}/etc/systemd/system/"
+install -v -m 600 files/tmux.service            	"${ROOTFS_DIR}/etc/systemd/system/"
 
 on_chroot << EOF
     systemctl enable labgrid-coordinator.service
